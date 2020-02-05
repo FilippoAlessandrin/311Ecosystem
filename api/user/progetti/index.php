36c051a0cc92bbaab7bcae80489c61a367f260a3
Andrea Mirandola <andrea.mirandola.99@gmail.com>
2020-02-05 15:43:11 +0100

    oh alessandrin io provo

---
+++ api/user/progetti/index.php
@@ -0,0 +1,7 @@
+<?php
+      include_once $_SERVER['DOCUMENT_ROOT']."/311Ecosystem/progetti/";
+
+      $ProjectController =new ProjectController();
+   
+       echo $ProjectController->insert();
+?>
