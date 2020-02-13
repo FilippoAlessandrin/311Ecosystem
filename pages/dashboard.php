1ffc1b3598b630430a9e4994d83e8927a5256540
Andrea Mirandola <andrea.mirandola.99@gmail.com>
2020-02-13 16:56:31 +0100

    collegamento calendario a prenotazione aule

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -141,7 +141,7 @@
 						</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="charts/chartjs.html">
+						<a class="nav-link" href="pages/calendar.php">
 							<i class="ti-calendar menu-icon"></i>
 							<span class="menu-title">Prenotazione Aule</span>
 						</a>
